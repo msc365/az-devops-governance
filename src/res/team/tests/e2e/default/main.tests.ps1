a45fc4129483be4d552942d3c75b452a2d182b07
Martin Swinkels <martin.swinkels@msc365.eu>
2025-11-25 13:51:16 +0000

    feat: Add end-to-end tests for Azure DevOps Team creation, removal, and update

---
+++ src/res/team/tests/e2e/default/main.tests.ps1
@@ -0,0 +1,25 @@
+﻿# ========== #
+# Parameters #
+# ========== #
+
+$params = @{
+    Organization = 'my-org'
+    ProjectId    = 'my-project'
+    TeamId       = 'my-other-team'
+    Description  = 'My other team description'
+}
+
+# ========= #
+# Variables #
+# ========= #
+
+$rootPath = (Get-Item $PSScriptRoot).Parent.Parent.Parent.FullName
+
+$organization = ((Get-AdoContext).Organization).Split('/')[-1]
+$params['Organization'] = $organization
+
+# ============== #
+# Test Execution #
+# ============== #
+
+& (Join-Path $rootPath -ChildPath 'main.ps1') @params
---
+++ src/res/team/tests/e2e/update/main.tests.ps1
@@ -0,0 +1,39 @@
+﻿# ========== #
+# Parameters #
+# ========== #
+
+$params = @{
+    Organization = 'my-org'
+    ProjectId    = 'my-project'
+    TeamId       = 'my-other-team'
+    Description  = 'My other team description updated'
+    Settings     = @{
+        bugsBehavior          = 'asRequirements'
+        backlogVisibilities   = @{
+            'Microsoft.EpicCategory'        = $true
+            'Microsoft.FeatureCategory'     = $true
+            'Microsoft.RequirementCategory' = $true
+        }
+        defaultIterationMacro = '@currentIteration'
+        workingDays           = @(
+            'monday'
+            'tuesday'
+            'wednesday'
+        )
+    }
+}
+
+# ========= #
+# Variables #
+# ========= #
+
+$rootPath = (Get-Item $PSScriptRoot).Parent.Parent.Parent.FullName
+
+$organization = ((Get-AdoContext).Organization).Split('/')[-1]
+$params['Organization'] = $organization
+
+# ============== #
+# Test Execution #
+# ============== #
+
+& (Join-Path $rootPath -ChildPath 'main.ps1') @params
