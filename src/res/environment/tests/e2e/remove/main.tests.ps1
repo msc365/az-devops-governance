de5f461021e5b84a5416fdb0fc13a954483317e2
Martin Swinkels <martin.swinkels@msc365.eu>
2025-11-30 10:53:34 +0000

    feat: Add end-to-end tests for environment management with parameters for resource group and update scenarios

---
+++ src/res/environment/tests/e2e/remove/main.tests.ps1
@@ -0,0 +1,24 @@
+﻿# ========== #
+# Parameters #
+# ========== #
+
+$params = @{
+    Name           = 'my-project-prd'
+    SubscriptionId = '00000000-0000-0000-0000-000000000000'
+    Remove         = $true
+}
+
+# ========= #
+# Variables #
+# ========= #
+
+$rootPath = (Get-Item $PSScriptRoot).Parent.Parent.Parent.FullName
+
+$subscriptionId = (Get-AdoContext).Subscription.Id
+$params['SubscriptionId'] = $subscriptionId
+
+# ============== #
+# Test Execution #
+# ============== #
+
+& (Join-Path $rootPath -ChildPath 'main.ps1') @params
---
+++ src/res/environment/tests/e2e/update/main.tests.ps1
@@ -0,0 +1,36 @@
+﻿# ========== #
+# Parameters #
+# ========== #
+
+$params = @{
+    Name           = 'my-project-prd'
+    SubscriptionId = '00000000-0000-0000-0000-000000000000'
+    ResourceGroup  = @{
+        Name     = 'rg-my-project-prd-weu'
+        Location = 'westeurope'
+        Tags     = @{
+            public      = 'false'
+            service     = 'my-project'
+            environment = 'prd'
+            security    = 'rbac'
+            iac         = 'bicep'
+            ci          = 'azure-pipelines'
+            test        = 'updated' # << ADD TAG >>
+        }
+    }
+}
+
+# ========= #
+# Variables #
+# ========= #
+
+$rootPath = (Get-Item $PSScriptRoot).Parent.Parent.Parent.FullName
+
+$subscriptionId = (Get-AdoContext).Subscription.Id
+$params['SubscriptionId'] = $subscriptionId
+
+# ============== #
+# Test Execution #
+# ============== #
+
+& (Join-Path $rootPath -ChildPath 'main.ps1') @params
