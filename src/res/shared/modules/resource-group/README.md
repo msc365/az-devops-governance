f3958cc91827f315fc7f8c192629066d04072e40
Martin Swinkels <martin.swinkels@msc365.eu>
2025-12-14 11:27:02 +0000

    feat: Add Resource Groups shared module with parameters

---
+++ src/res/shared/modules/resource-group/README.md
@@ -0,0 +1,76 @@
+<!-- markdownlint-disable no-duplicate-heading -->
+<!-- omit from toc -->
+# Resource Group `[res\shared\modules\resource-group\main.ps1]`
+
+Create or update an Azure Resource Group.
+
+<!-- omit from toc -->
+## Navigation
+
+- [Description](#description)
+- [Parameters](#parameters)
+- [Examples](#examples)
+- [Outputs](#outputs)
+- [Support](#support)
+- [Notes](#notes)
+
+## Description
+
+This script creates a new Azure Resource Group or updates an existing one with specified tags.
+
+## Parameters
+
+| Parameter | Type | Required | Default | Description |
+| :-- | :-- | :-- | :-- | :-- |
+| `Name` | `String` | Yes | `-` | Required. The name of the Resource Group. |
+| `Force` | `SwitchParameter` | No | `-` | Optional. Skip confirmation prompt and proceed with operations immediately. |
+| `Location` | `String` | No | `'westeurope'` | Optional. The Azure region where the Resource Group will be created. Defaults to 'westeurope'. |
+| `Rollback` | `SwitchParameter` | No | `-` | Optional. If specified, the script will not delete or modify the Resource Group. |
+| `Tags` | `Object` | No | `-` | Optional. A hashtable of tags to assign to the Resource Group. |
+
+## Examples
+
+### Example 1
+
+#### PowerShell
+
+```powershell
+$rgParams = @{
+    Name     = 'rg-my-resource-group'
+    Location = 'westeurope'
+    Tags     = @{
+        'environment' = 'prd'
+        'owner'       = 'e2egov'
+    }
+    Verbose  = $true
+}
+.\main.ps1 @rgParams
+```
+
+Creates or updates the Resource Group 'rg-my-resource-group' in the 'westeurope' region with the specified tags.
+
+
+## Outputs
+
+Returns: `pscustomobject`
+
+## Support
+
+### CommonParameters
+
+This cmdlet supports the common parameters: `-Debug`, `-ErrorAction`, `-ErrorVariable`,  
+`-InformationAction`, `-InformationVariable`, `-OutBuffer`, `-OutVariable`, `-PipelineVariable`,  
+`-ProgressAction`, `-Verbose`, `-WarningAction`, and `-WarningVariable`. For more information, see  
+[about_CommonParameters](https://go.microsoft.com/fwlink/?LinkID=113216).
+
+### SupportsShouldProcess
+
+This script supports the `-WhatIf` and `-Confirm` parameters for safe execution:
+
+- **`-WhatIf`**: Shows what would happen if the script runs without actually making any changes.
+- **`-Confirm`**: Prompts for confirmation before performing each action.
+
+
+## Notes
+
+Does not perform actual resource group deletion despite the Remove-AzResourceGroup reference due to the current implementation focusing on creation and updating tags only.
---
+++ src/res/shared/modules/resource-group/main.ps1
@@ -0,0 +1,151 @@
+﻿<#
+.SYNOPSIS
+    Create or update an Azure Resource Group.
+
+.DESCRIPTION
+    This script creates a new Azure Resource Group or updates an existing one with specified tags.
+
+.PARAMETER Name
+    Required. The name of the Resource Group.
+
+.PARAMETER Location
+    Optional. The Azure region where the Resource Group will be created. Defaults to 'westeurope'.
+
+.PARAMETER Tags
+    Optional. A hashtable of tags to assign to the Resource Group.
+
+.PARAMETER Rollback
+    Optional. If specified, the script will not delete or modify the Resource Group.
+
+.PARAMETER Force
+    Optional. Skip confirmation prompt and proceed with operations immediately.
+
+.EXAMPLE
+    $rgParams = @{
+        Name     = 'rg-my-resource-group'
+        Location = 'westeurope'
+        Tags     = @{
+            'environment' = 'prd'
+            'owner'       = 'e2egov'
+        }
+        Verbose  = $true
+    }
+    .\main.ps1 @rgParams
+
+    Creates or updates the Resource Group 'rg-my-resource-group' in the 'westeurope' region with the specified tags.
+
+.NOTES
+    Does not perform actual resource group deletion despite the Remove-AzResourceGroup reference due to
+    the current implementation focusing on creation and updating tags only.
+#>
+[CmdletBinding(SupportsShouldProcess)]
+[OutputType([pscustomobject])]
+param (
+    [Parameter(Mandatory)]
+    [string]$Name,
+
+    [Parameter(Mandatory = $false)]
+    [string]$Location = 'westeurope',
+
+    [Parameter(Mandatory = $false)]
+    [object]$Tags,
+
+    [Parameter()]
+    [switch]$Rollback,
+
+    [Parameter()]
+    [switch]$Force
+)
+
+begin {
+    Write-Verbose ('[Enter]: .\src\res\shared\modules\resource-group\{0}' -f $MyInvocation.MyCommand.Name)
+}
+
+process {
+    try {
+        $ErrorActionPreference = 'Stop'
+
+        #region INITIALIZE
+
+        # Variables
+        $rg = $null
+
+        # Resource Group
+        $rg = Get-AzResourceGroup -Name $Name -ErrorAction SilentlyContinue
+
+        #endregion
+
+        #region DEPLOYMENT
+
+        if (-not $Rollback.IsPresent) {
+            if ($null -eq $rg) {
+                if ($PSCmdlet.ShouldProcess("Call module 'Az.Resources' operation.", 'New-AzResourceGroup')) {
+                    $rgSplat = @{
+                        Name     = $Name
+                        Location = $Location
+                        Tags     = $Tags
+                        Verbose  = $VerbosePreference
+                    }
+
+                    $rg = New-AzResourceGroup @rgSplat -ErrorAction Stop
+                }
+            } else {
+                # Check if tags differ
+                $tagsDiff = $false
+                foreach ($key in $Tags.Keys) {
+                    if ($rg.Tags.Count -ne $Tags.Count -or
+                        -not $rg.Tags.ContainsKey($key) -or
+                        $rg.Tags[$key] -ne $Tags[$key]) {
+
+                        $tagsDiff = $true
+                        break
+                    }
+                }
+
+                # Update tags if they differ
+                if ($tagsDiff) {
+                    if ($PSCmdlet.ShouldProcess("Call module 'Az.Resources' operation.", 'Set-AzResourceGroup')) {
+                        $rgSplat = @{
+                            Name    = $Name
+                            Tags    = $Tags
+                            Verbose = $VerbosePreference
+                        }
+
+                        $rg = Set-AzResourceGroup @rgSplat -ErrorAction Stop
+                    }
+                } else {
+                    Write-Verbose ("Exists. '/resourceGroup/{0}'" -f $rg.ResourceGroupName)
+                }
+            }
+        }
+
+        #endregion
+
+        #region ROLLBACK
+
+        if ($Rollback.IsPresent) {
+            if ($null -eq $rg) {
+                Write-Verbose ("Doesn't exist. '/resourceGroup/{0}'" -f $Name)
+            } else {
+                if ($PSCmdlet.ShouldProcess("None 'Az.Resources' operation.", 'Remove-AzResourceGroup')) {
+                    Write-Verbose ("None. Will not remove '/resourceGroup/{0}'" -f $rg.ResourceGroupName)
+                }
+            }
+        }
+
+        #endregion
+
+        #region OUTPUT
+
+        return $rg
+
+        #endregion
+
+    } catch {
+        throw $_
+    }
+}
+
+end {
+    Write-Verbose ('[Exit]: .\src\res\shared\modules\resource-group\{0}' -f $MyInvocation.MyCommand.Name)
+}
