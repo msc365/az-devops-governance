0b539b9497835deb363ef274dcab47b6fab104f5
Martin Swinkels <martin.swinkels@msc365.eu>
2026-02-01 13:49:13 +0100

    feat(ConvertTo-Hashtable): Add ConvertTo-Hashtable function for object conversion to hashtable

---
+++ src/utl/ConvertTo-Hashtable.ps1
@@ -0,0 +1,20 @@
+﻿function ConvertTo-Hashtable {
+    param(
+        [object]$InputObject
+    )
+
+    switch ($InputObject) {
+        { $_ -is [System.Collections.IDictionary] } { return $_ }
+        { $_ -is [System.Collections.IEnumerable] -and -not ($_ -is [string]) } {
+            return @(@( $_ | ForEach-Object { ConvertTo-Hashtable $_ } ))
+        }
+        { $_ -is [pscustomobject] } {
+            $hash = [ordered]@{}
+            foreach ($prop in $_.PSObject.Properties) {
+                $hash[$prop.Name] = ConvertTo-Hashtable $prop.Value
+            }
+            return $hash
+        }
+        default { return $_ }
+    }
+}
